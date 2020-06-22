ca3e54628b2372c3ddee77d8dae46fc047726930
fkaraagac11 <fehmi11.qa@gmail.com>
2020-06-22 18:05:35 -0400

    fc

---
+++ EventEmitter.js
@@ -0,0 +1,33 @@
+const Emitter = require("events");
+const eventEmitter = new Emitter();
+
+eventEmitter.on("tutorial", (num1, num2) => {
+    console.log(num1 + num2);
+});
+
+eventEmitter.emit("tutorial", 2, 3);
+
+class Person extends Emitter {
+    constructor(name) {
+        super();
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
+
+let fehmi = new Person("fehmi");
+let melih = new Person("melih");
+
+fehmi.on("name", () => {
+    console.log("my name is " + fehmi.name);
+});
+
+melih.on("name", () => {
+    console.log("my name is " + melih.name);
+});
+
+fehmi.emit("name");
+melih.emit("name");
