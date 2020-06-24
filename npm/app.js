79e6320e6b34e9482d910e8daeb76403c20c311c
fkaraagac11 <fehmi11.qa@gmail.com>
2020-06-24 00:48:34 -0400

    fc

--- npm/app.js
+++ npm/app.js
@@ -1,8 +1,27 @@
-const Emitter = require("events");
-eventEmitter = new Emitter();
+//Events module
+// const Emitter = require("events");
+// eventEmitter = new Emitter();
 
-eventEmitter.on("message", function (name) {
-    console.log(name);
+// eventEmitter.on("message", function (name) {
+//     console.log(name);
+// });
+
+// eventEmitter.emit("message", "fehmi");
+
+// URL HTTP
+
+const express = require("express");
+const { response } = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+    if (err) {
+        response.status(500).send("sorry out of scope");
+    }
+    response.send(html);
 });
 
-eventEmitter.emit("message", "fehmi");
+app.listen(process.env.PORT || 3000, () =>
+    console.log(`App available on http://localhost/:3000`)
+);
