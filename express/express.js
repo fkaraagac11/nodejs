85986c58c5391c616a602b57d0a7f68e35e63af1
fkaraagac11 <fehmi11.qa@gmail.com>
2020-06-26 22:28:34 -0400

    new

--- express/express.js
+++ express/express.js
@@ -5,4 +5,6 @@ app.get("/", (req, res) => {
     res.send("An alligator approaches!");
 });
 
-app.listen(3000, () => console.log("Gator app listening on port 3000!"));
+app.listen(3000, function () {
+    console.log("Gator app listening on port 3000!");
+});
